42052966ab3046af48864ac11bb55d7d770116ae
javiergarridomellado <javiergarridomellado@correo.ugr.es>
2015-12-06 13:26:56 +0100

    deploy heroku

--- heroku_deploy.sh
+++ heroku_deploy.sh
@@ -2,7 +2,7 @@ wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh   # descargar herram
 heroku login
 heroku create
 git add .
-git commit -m "despliegue en heroku"
+git commit -m "heroku deploy"
 git push heroku master
 heroku ps:scale web=1
 heroku open
