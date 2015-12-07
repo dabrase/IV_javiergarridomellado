83c5c24ddca46c7e59eb86366ed776909b989109
javiergarridomellado <javiergarridomellado@correo.ugr.es>
2015-12-07 11:43:50 +0100

    script docker,heroku and run

---
+++ script/docker_install_and_run.sh
@@ -0,0 +1,4 @@
+sudo apt-get update
+sudo apt-get install -y docker.io
+sudo docker pull javiergarridomellado/iv_javiergarridomellado:apuestas
+sudo docker run -t -i javiergarridomellado/iv_javiergarridomellado:apuestas /bin/bash
