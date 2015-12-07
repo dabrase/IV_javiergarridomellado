a416d9db359851e0ae7753ba5b755a122897c1e0
javiergarridomellado <javiergarridomellado@correo.ugr.es>
2015-12-07 13:27:08 +0100

    borrando

--- script/docker_install_and_run.sh
+++
@@ -1,4 +0,0 @@
-sudo apt-get update
-sudo apt-get install -y docker.io
-sudo docker pull javiergarridomellado/iv_javiergarridomellado:apuestas
-sudo docker run -t -i javiergarridomellado/iv_javiergarridomellado:apuestas /bin/bash
