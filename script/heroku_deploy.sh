22e97de2469c1a9787fb31ce93b8b756d750fddc
javiergarridomellado <javiergarridomellado@correo.ugr.es>
2015-12-07 11:08:23 +0100

    script

---
+++ script/heroku_deploy.sh
@@ -0,0 +1,11 @@
+sudo apt-get install wget
+wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh   # descargar herramienta heroku CLI
+heroku login
+heroku create
+git add .
+git commit -m "heroku deploy"
+git push heroku master
+heroku run python manage.py syncdb --noinput
+heroku ps:scale web=1
+heroku apps:rename ejemplodeploy
+heroku open
