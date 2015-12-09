78649b3b2f5ffd709608746d64aa27f5a38c2760
javiergarridomellado <javiergarridomellado@correo.ugr.es>
2015-12-09 11:01:07 +0100

    Install library

---
+++ scripts/install_library.sh
@@ -0,0 +1,14 @@
+sudo apt-get install -y python-setuptools
+sudo apt-get -y install python-dev
+sudo apt-get -y install build-essential
+sudo apt-get -y install python-psycopg2
+sudo apt-get -y install libpq-dev
+sudo easy_install pip
+sudo pip install --upgrade pip
+
+#Instalación de dependencias de la aplicacion
+#cd ..
+#sudo pip install -r requirements.txt
+
+#Sincronización de la base de datos
+#sudo python manage.py syncdb --noinput
